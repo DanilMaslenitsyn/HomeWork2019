14e9efd813f751cd52c568686278b87fb0530741
DanilMaslenitsyn <airpronn@ya.ru>
2019-07-08 16:31:13 +0300

    Дз к уроку № 1

---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача № 1
+
+            //Console.WriteLine("Доброго времяни суток.");
+
+            //Console.WriteLine("Введите ваше Имя.");
+            //string naem = Console.ReadLine();
+
+            //Console.WriteLine("Введите вашу Фамилию.");
+
+            //string surname = Console.ReadLine();
+
+            //Console.WriteLine("Введите ваш Возраст.");
+            //int age = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Введите ваш Рост в метрах");
+            //double weight = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите ваш Вес.");
+            //int height = Convert.ToInt32(Console.ReadLine());
+
+            //A)
+            //Console.WriteLine("Имя: "+ naem + " Фамилия: " + surname + " Возраст: " + age + " Рост: "+ weight + " Вес: " + height);
+            //B)
+            //Console.WriteLine("Имя : {0}, Фамилия: {1}, Возраст:{2}, Рост:{3}, Вес:{4}",
+            //naem,
+            //surname,
+            //age,
+            //weight,
+            //height);
+            //C)
+            //Console.WriteLine("");
+            //Console.WriteLine($"Имя : {naem} Фамилия: {surname} Возраст:{age} Рост:{weight} Вес:{height}");
+            //Console.WriteLine("");
+
+            //Задача № 2
+
+            //Console.WriteLine("Расчетать индекс массы вашего тела " + naem + " " + surname + ". ");
+            //Console.ReadLine();
+
+
+            //double I = height / Math.Pow(weight, 2);
+            // double I = (double)(height / (weight * weight));
+
+            //Console.WriteLine("Индекс массы вашего тела: " + I);
+            //Console.ReadLine();
+            //Задание 3
+
+
+            //Console.WriteLine("x 1 ="); double x1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("y 1 ="); double y1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("x 2 ="); double x2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("y 2 ="); double y2 = Convert.ToDouble(Console.ReadLine());
+            //double r = Distance(x1, y1, x2, y2);
+            //Console.WriteLine($"r = {r} ");
+            //Console.WriteLine("{0:F2}", r);
+            //Console.ReadLine();
+
+            //Задание 5
+
+            //Console.WriteLine("Доброго времяни суток.");
+
+            //Console.WriteLine("Введите ваше Имя.");
+            //string naem = Console.ReadLine();
+
+            //Console.WriteLine("Введите вашу Фамилию.");
+
+            //string surname = Console.ReadLine();
+
+            //Console.WriteLine("Введите ваш Город.");
+            //string city = Console.ReadLine();
+
+            //string txt = $"Имя : {naem} Фамилия: {surname} Город: {city}";
+
+            //Console.SetCursorPosition(Console.WindowWidth / 2 - txt.Length / 2, Console.WindowHeight / 2);
+            //Console.WriteLine(txt);
+
+            //Console.ReadLine();
+        }
+       
+
+
+
+        //private static double Distance(double x1, double y1, double x2, double y2)
+        //{
+        //    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        
+            // Задание 4
+
+            //int a = 1;
+            //int b = 2;
+            //Console.WriteLine($"a = {a}, b = {b}  ");
+
+            //int c = a;
+            //a = b;
+            //b = c;
+
+            //a = a + b;
+            //b = a - b;
+            //a = a - b;
+            //Console.WriteLine($"a = {a}, b = {b}  ");
+
+            //Console.ReadLine();
+           
+        }
+    }
+}
